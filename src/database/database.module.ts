c6a37b391424bf963cee2df70b5560f914339b16
Davi Batista <davideosmar11@gmail.com>
2024-07-16 00:09:24 -0300

    feat: added basic project data and configurations

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { appConfigurations } from 'src/shared/config/app.config';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        type: appConfigurations.DB_ENGINE as any,
+        host: appConfigurations.DB_HOST,
+        port: appConfigurations.DB_PORT,
+        username: appConfigurations.DB_USER,
+        password: appConfigurations.DB_PASSWORD,
+        database: appConfigurations.DB_DATABASE,
+        entities: [appConfigurations.DB_ENTITIES],
+        synchronize: appConfigurations.DB_SYNCHRONIZE,
+        logging: appConfigurations.DB_LOGGING,
+        logger: 'advanced-console',
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
---
+++ src/shared/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const sharedSwaggerConfig = new DocumentBuilder()
+  .setTitle("Wits")
+  .setDescription("Documentação dos endpoints do projeto Wits, um site de quiz gaming para raciocínio lógico e aritmético.")
+  .setVersion('1.0.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      description: 'Insira um token JWT para autenticar a requisição.',
+    },
+    'user-token',
+  )
+  .build();
