2a862116a041bf84ad828e2adf4c08e779e905e1
Davi Batista <davideosmar11@gmail.com>
2024-07-17 13:01:59 -0300

    fix: fixed duplicated provider folder on other modules

--- src/modules/user-activities-answered/domain/providers/jwt.provider.ts
+++
@@ -1,7 +0,0 @@
-import { JwtPayload } from 'jsonwebtoken';
-import { JWTProviderDTO, JWTValidateDTO } from '../../dto/JWTProvider.dto';
-
-export interface JWTProviderInterface {
-  generate(data: JWTProviderDTO): string;
-  validate(data: JWTValidateDTO): JwtPayload | string;
-}
--- src/modules/user-score/domain/providers/hash.provider.ts
+++
@@ -1,4 +0,0 @@
-export interface HashProviderInterface {
-  hash(payload: string): Promise<string>;
-  compare(payload: string, hash: string): Promise<boolean>;
-}
