6ce258f47e675d7b39e94aa1aa9b4129ed25b9d0
Davi Batista <davideosmar11@gmail.com>
2024-07-17 12:16:27 -0300

    feat: created user's concluded courses entity

--- src/modules/user-courses-concluded/entity/user-courses-concluded.entity.ts
+++ src/modules/user-courses-concluded/entity/user-courses-concluded.entity.ts
@@ -1,1 +1,22 @@
-export class UserCoursesConcludedEntity {}
+import { Course } from "src/modules/course/entity/course.entity";
+import { User } from "src/modules/user/entity/user.entity";
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+
+@Entity()
+class UserCoursesConcluded {
+    @PrimaryColumn()
+    user_id: number;
+
+    @PrimaryColumn()
+    course_id: number;
+
+    @ManyToOne(() => User, (user) => user.id_user)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Course, (course) => course.id_course)
+    @JoinColumn({ name: 'course_id' })
+    course: Course;
+}
+
+export { UserCoursesConcluded as UserCourseConcluded }
