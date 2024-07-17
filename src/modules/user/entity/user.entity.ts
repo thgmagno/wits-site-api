733ed727efee80190254da4e883192a1530e1b4b
Davi Batista <davideosmar11@gmail.com>
2024-07-17 11:44:11 -0300

    feat: created user entity

---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,54 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export enum Role {
+    ADMIN = 'admin',
+    COMMON = 'common',
+}
+
+@Entity()
+class Users {
+    @PrimaryGeneratedColumn()
+    id_user: number;
+
+    @Column({
+        unique: true,
+        length: 35,
+      })
+      username: string;
+
+      @Column({
+        unique: true,
+        length: 50,
+      })
+      email: string;
+
+      @Column({
+        length: 500,
+      })
+      password: string;
+
+      @Column({
+        name: 'role',
+        type: 'enum',
+        enum: [Role.ADMIN, Role.COMMON],
+        enumName: 'role',
+      })
+      role: string;
+    
+      @Column({
+        nullable: true,
+      })
+      deleted_at: string;
+    
+      @CreateDateColumn({
+        nullable: false,
+      })
+      created_at: Date;
+    
+      @UpdateDateColumn({
+        nullable: false,
+      })
+      updated_at: Date;
+}
+
+export { Users as User };
