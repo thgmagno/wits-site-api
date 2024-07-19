2f3b57b5ab3db2aecb95451ef18cfcf2fa00856f
Davi Batista <davideosmar11@gmail.com>
2024-07-19 20:58:50 -0300

    feat: created courses seeder

--- src/modules/Main.seeder.ts
+++ src/modules/Main.seeder.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager, runSeeder } from 'typeorm-extension';
+import CourseSeeder from './course/seeders/course.seeder';
 
 export class MainSeeder implements Seeder {
   async run(
@@ -7,6 +8,6 @@ export class MainSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<void> {
     // Seeders will go here
-    
+    await runSeeder(dataSource, CourseSeeder);
   }
 }
