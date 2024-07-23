0d1c826a79b5251827f34b856ab88f6e1cd6847d
Davi Batista <davideosmar11@gmail.com>
2024-07-23 11:10:24 -0300

    feat: created activity edition endpoint

---
+++ src/modules/activity/domain/requests/EditActivity.request.dto.ts
@@ -0,0 +1,31 @@
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'nestjs-zod/z';
+
+export const EditActivityRequestSchema = z.object({
+    course_id: z.number().int().positive().describe('ID do Curso'),
+    question: z.string().max(255).describe('Pergunta'),
+    option_1: z.string().max(600).describe('Opção 1'),
+    option_2: z.string().max(600).describe('Opção 2'),
+    option_3: z.string().max(600).describe('Opção 3'),
+    option_4: z.string().max(600).describe('Opção 4'),
+    correct_answer: z.string().max(1).describe('Resposta Correta'),
+})
+
+export class EditActivityRequestDTO extends createZodDto(
+    EditActivityRequestSchema,
+) {}
+
+export const EditActivityResponseSchema = z.object({
+    course_id: z.number().int().positive().describe('ID do Curso'),
+    question: z.string().max(255).describe('Pergunta'),
+    option_1: z.string().max(600).describe('Opção 1'),
+    option_2: z.string().max(600).describe('Opção 2'),
+    option_3: z.string().max(600).describe('Opção 3'),
+    option_4: z.string().max(600).describe('Opção 4'),
+    correct_answer: z.string().max(1).describe('Resposta Correta'),
+    created_at: z.date().describe('Data de Lançamento'),
+})
+
+export class EditActivityResponseDTO extends createZodDto(
+    EditActivityResponseSchema,
+) {}
--- src/modules/activity/services/activity.service.ts
+++ src/modules/activity/services/activity.service.ts
@@ -4,6 +4,8 @@ import { Activity } from '../entity/activity.entity';
 import { CourseNotFoundException } from '../../course/domain/errors/CourseNotFound.exception';
 import { CourseRepository } from '../../course/repository/course.repository';
 import { CreateActivityRequestDTO, CreateActivityResponseDTO } from '../domain/requests/CreateActivity.request.dto';
+import { EditActivityRequestDTO, EditActivityResponseDTO } from '../domain/requests/EditActivity.request.dto';
+import { ActivityNotFoundException } from '../domain/errors/ActivityNotFound.exception';
 
 @Injectable()
 export class ActivityService {
@@ -25,6 +27,18 @@ export class ActivityService {
         });
 
         if (!course) throw new CourseNotFoundException();
+
+        return {
+            ...await this.activityRepository.save(activityData)
+        }
+    }
+
+    async editActivity(id: number, activityData: EditActivityRequestDTO): Promise<EditActivityResponseDTO | ActivityNotFoundException> {
+        const activity = await this.activityRepository.findOne({
+            where: { id_activity: id },
+        });
+
+        if (!activity) throw new ActivityNotFoundException();
 
         return {
             ...await this.activityRepository.save(activityData)
